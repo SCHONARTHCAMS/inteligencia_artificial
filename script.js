d44fdbde6feda21bd9b808f84e1742adfa6dbd31
Schonarthcams <105643168+SCHONARTHCAMS@users.noreply.github.com>
2024-08-08 10:08:19 -0300

    salvando

--- script.js
+++ script.js
@@ -19,41 +19,41 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Depois disso a inteligência te mostra novas formas de utilizar a IA. Como você reage a isso?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Isso é incrivel!",
+                afirmacao: "Fico animada e presto atenção no que o robô fala."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Isso é muito perigoso!",
+                afirmacao: "Não sei e goto muito dessa inovação, mas presto atenção mesmo assim."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após sua aula de jazz você vai para a ecola e a profesora propõe que você faça um trabalho, como você vai fazer?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Vou utilizar o chat GPT, seguindo o que disse o robô.",
+                afirmacao: "Faço o trabalho com os conselhos que o robô me deu, e finalizo rapidamente."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Pesquiso em livros, pois não confio no robô.",
+                afirmacao: "Demoro bastante para finaliar a atividade, perdendo uma boa parte do recreio."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Na hora do recreio você e seus amigos começam a conversar sobre o que acham da inteligencia artificial, qual sua opinião?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Falo que gosto da IA e defendo o uso dela. ",
+                afirmacao: "Argumento falando que a IA pode revolucionar o mundo e nos ajudar positivamente."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Falo que não gosto da IA e argumento contra ela.",
+                afirmacao: "Argumento falando que a IA pode tirar diveros empregos e que podemos perder o controle sob ela, causando um colapso. "
             }
         ]
     },
